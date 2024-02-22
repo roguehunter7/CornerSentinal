683cbcbf4609ae48987846486970c53046089ca3
Sreeram K R <krsreeram007@gmail.com>
2024-02-22 23:49:30 +0530

    function object error fix #1

--- newright.py
+++ newright.py
@@ -155,7 +155,7 @@ while cap.isOpened():
                         # Receive binary_code from the other Raspberry Pi
                         recv_binary_code = receive_binary_data
                         # Trigger transmit_binary_data function (assuming it's defined in the send module)
-                        transmit_binary_data(recv_binary_code)
+                        transmit_binary_data(str(recv_binary_code))
                         
                         display_warning_message(annotated_frame, binary_code)
                         cv2.putText(annotated_frame, f"Speed: {speed:.2f} km/h", (int(x), int(y) - 10),
