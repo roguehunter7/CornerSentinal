4792d547f9d4f36888069203bb38021ec0f11690
Sreeram K R <krsreeram007@gmail.com>
2024-02-25 21:47:49 +0530

    Update msgsend.c

--- msgsend.c
+++ msgsend.c
@@ -10,22 +10,23 @@ int counter = 20;
 void chartobin(char c)
 {
     int i;
-    for (i = 7; i >= 0; i--)
-    {
-        result[counter] = (c & (1 << i)) ? '1' : '0';
+    for(i=7; i>=0;i--){
+        result[counter]= (c&(1<<i))?'1':'0';  
         counter++;
     }
 }
 
 void int2bin(unsigned integer, int n)
-{
-    for (int i = 0; i < n; i++)
+{  
+    for (int i=0;i<n;i++)   
     {
-        result[counter] = (integer & (int)1 << (n - i - 1)) ? '1' : '0';
-        result[36] = '\0';
-        counter++;
+    result[counter]= (integer & (int)1<<(n-i-1)) ? '1' : '0';
+    result[36]='\0';
+    counter++;
     }
+    
 }
+
 int pos = 0;
 int main()
 {
@@ -46,31 +47,19 @@ int main()
         gpiod_chip_close(chip);
         return -1;
     }
-
-    // Configure the GPIO line
-    if (gpiod_line_request_output(line, "led-control", 0) < 0)
-    {
-        perror("Error configuring GPIO line");
-        gpiod_line_release(line);
-        gpiod_chip_close(chip);
-        return -1;
-    }
     struct timeval tval_before, tval_after, tval_result;
     // Read message
-    char msg[3000];
+    char msg[3000]; 
     int len, k, length;
-
-    printf("\nEnter the Message: ");
-    scanf("%[^'\n']", msg);
-
-    len = strlen(msg);
-
-    int2bin(len * 8, 16);
-    printf("Frame Header (Synchro and Textlength = %s\n", result);
-
-    for (k = 0; k < len; k++)
-    {
-        chartobin(msg[k]);
+       
+    printf("\n Enter the Message: ");
+    scanf("%[^'\n']",msg);
+    len=strlen(msg);    
+    int2bin(len*8, 16); 
+    printf ("Frame Header (Synchro and Textlength = %s\n", result);
+        
+    for(k=0;k<len;k++){
+        chartobin(msg[k]);            
     }
 
     length = strlen(result);
@@ -99,6 +88,7 @@ int main()
             gpiod_line_set_value(line, 0); // Set GPIO line to LOW
             pos++;
             }
+        gpiod_line_set_value(line, 0); // Set GPIO line to LOW  
     }
 
     // Cleanup libgpiod
