a2f9468d388a5accc94d2649f8f023c657b4f76f
Sreeram K R <krsreeram007@gmail.com>
2024-02-25 23:34:23 +0530

    Update msgsend.c

--- msgsend.c
+++ msgsend.c
@@ -4,10 +4,14 @@
 #include <sys/time.h>
 #include <gpiod.h>
 
+#define PREAMBLE_LEN 20
+#define PAYLOAD_SIZE_LEN 16
+#define PAYLOAD_LEN 2500
+
 typedef struct {
-    char preamble[20];
-    char payload_size[16];
-    char payload[2500];
+    char preamble[PREAMBLE_LEN];
+    char payload_size[PAYLOAD_SIZE_LEN];
+    char payload[PAYLOAD_LEN];
 } wiremsg;
 
 void int2bin(unsigned integer, int n, char *result, int *counter) {
@@ -18,33 +22,29 @@ void int2bin(unsigned integer, int n, char *result, int *counter) {
 }
 
 void setHeaderPayloadSizeField(unsigned msgLen, wiremsg *lifiWireMsg_p) {
-    int payload_size_filed_len = sizeof(lifiWireMsg_p->payload_size);
-    int counter = 0;
-    for (int i = 0; i < payload_size_filed_len; i++) {
-        lifiWireMsg_p->payload_size[i] = (msgLen & (int)1 << (payload_size_filed_len - i - 1)) ? '1' : '0';
+    for (int i = 0; i < PAYLOAD_SIZE_LEN; i++) {
+        lifiWireMsg_p->payload_size[i] = (msgLen & (int)1 << (PAYLOAD_SIZE_LEN - i - 1)) ? '1' : '0';
     }
-    lifiWireMsg_p->payload_size[payload_size_filed_len] = '\0';
+    lifiWireMsg_p->payload_size[PAYLOAD_SIZE_LEN] = '\0';
 }
 
 void setPayload(unsigned msgLen, wiremsg *lifiWireMsg_p, char *input_buffer) {
     int counter = 0;
     for (int i = 0; i < msgLen; i++) {
         for (int j = 7; j >= 0; j--) {
-            lifiWireMsg_p->payload[counter] = (input_buffer[i] & (1 << j)) ? '1' : '0';
-            counter++;
+            lifiWireMsg_p->payload[counter++] = (input_buffer[i] & (1 << j)) ? '1' : '0';
         }
     }
 }
 
 int main() {
     struct timeval tval_before, tval_after, tval_result;
-    char input_buffer[2500];
+    char input_buffer[PAYLOAD_LEN];
     wiremsg lifiWireMsg = {.preamble = {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'}};
 
     struct gpiod_chip *chip;
     struct gpiod_line_bulk lines;
-    struct gpiod_line_event event;
-    unsigned int line_offset = 4;
+    unsigned int line_offset = 0;
     const char *consumer = "lifi_consumer";
 
     chip = gpiod_chip_open("/dev/gpiochip0");
@@ -57,18 +57,17 @@ int main() {
     gpiod_line_request_output_lines_bulk(&lines, consumer, 0);
 
     while (1) {
-        printf(" Please input the message to send: ");
+        printf("Please input the message to send: ");
         fgets(input_buffer, sizeof(input_buffer), stdin);
-        int msg_len = strlen(input_buffer);
+        int msg_len = strlen(input_buffer) - 1;  // Exclude newline character
         setHeaderPayloadSizeField(msg_len, &lifiWireMsg);
         setPayload(msg_len, &lifiWireMsg, input_buffer);
-        int bitPos = 0;
 
-        int wireMsgLen = sizeof(lifiWireMsg.preamble) + sizeof(lifiWireMsg.payload_size) + msg_len;
+        int wireMsgLen = PREAMBLE_LEN + PAYLOAD_SIZE_LEN + msg_len;
 
         gettimeofday(&tval_before, NULL);
 
-        while (bitPos < wireMsgLen) {
+        for (int bitPos = 0; bitPos < wireMsgLen; bitPos++) {
             gettimeofday(&tval_after, NULL);
             timersub(&tval_after, &tval_before, &tval_result);
             double time_elapsed = (double)tval_result.tv_sec + ((double)tval_result.tv_usec / 1000000.0f);
@@ -82,25 +81,18 @@ int main() {
             gettimeofday(&tval_before, NULL);
 
             char bitToSend;
-            if (bitPos < sizeof(lifiWireMsg.preamble)) {
+            if (bitPos < PREAMBLE_LEN) {
                 bitToSend = lifiWireMsg.preamble[bitPos];
-            } else if (bitPos < sizeof(lifiWireMsg.preamble) + sizeof(lifiWireMsg.payload_size)) {
-                bitToSend = lifiWireMsg.payload_size[bitPos - sizeof(lifiWireMsg.preamble)];
+            } else if (bitPos < PREAMBLE_LEN + PAYLOAD_SIZE_LEN) {
+                bitToSend = lifiWireMsg.payload_size[bitPos - PREAMBLE_LEN];
             } else {
-                bitToSend = lifiWireMsg.payload[bitPos - sizeof(lifiWireMsg.preamble) - sizeof(lifiWireMsg.payload_size)];
+                bitToSend = lifiWireMsg.payload[bitPos - PREAMBLE_LEN - PAYLOAD_SIZE_LEN];
             }
 
-            if (bitToSend == '1') {
-                gpiod_line_set_value_bulk(&lines, 1);
-            } else {
-                gpiod_line_set_value_bulk(&lines, 0);
-            }
-
-            bitToSend++;
+            gpiod_line_set_value_bulk(&lines, bitToSend == '1');
         }
     }
-     
-    gpiod_line_release(line);
+
     gpiod_chip_close(chip);
     return 0;
 }
