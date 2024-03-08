7d90e39d4238c42b4147b78145cf21433a0760f4
Sreeram K R <krsreeram007@gmail.com>
2024-03-08 21:09:34 +0530

    .

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -19,7 +19,7 @@ void setup() {
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0;
   // set timer count for 1khz increments
-  OCR1A = 2001;// = (16*10^6) / (250*8) - 1        OCR1A = 2001 for 1kHz; ORCIA = 7999 for 250Hz
+  OCR1A = 7999;// = (16*10^6) / (250*8) - 1        OCR1A = 2001 for 1kHz; ORCIA = 7999 for 250Hz
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS11 bit for 8 prescaler
@@ -32,6 +32,7 @@ void setup() {
   
   Serial.begin(9600);
 
+  analogReference(INTERNAL);
 
   //Input Pin for the Solarplate
   pinMode(A0,INPUT);
@@ -48,7 +49,7 @@ ISR(TIMER1_COMPA_vect)
   int sensorValue = analogRead(A0);
   float voltage = sensorValue * (5.0 / 1023.0);
   // Serial.println(sensorValue);
-  Serial.println(voltage);
+  // Serial.println(voltage);
 
   if (voltage > 1) 
   {
@@ -104,7 +105,7 @@ void lookForSynchro(String bit)
   String preambel="1010101111111111";
   sequence.concat(bit);
   sequence.remove(0,1);
-  //Serial.println("Sequence: "+sequence);
+  Serial.println("Sequence: "+sequence);
   if (sequence==preambel)
   {
     Serial.println("Synchro done");
