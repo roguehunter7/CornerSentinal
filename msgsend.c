b21e9456fba9a98fd5766ff24d3e20ff245f5020
Sreeram K R <krsreeram007@gmail.com>
2024-03-29 17:49:47 +0530

    Update msgsend.c

--- msgsend.c
+++ msgsend.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
 #include <sys/time.h>
+#include <string.h>
 #include <gpiod.h>
 
-#define GPIO_CHIP_NAME "gpiochip4"  // GPIO chip name
-#define GPIO_PIN_NUMBER 4            // GPIO pin number
+#define GPIO_CHIP_NAME "gpiochip4"  // Change this if your GPIO chip name is different
+#define GPIO_PIN_NUMBER 4           // Change this to your desired GPIO pin number
+#define BUFFER_SIZE 3000
 
-char result[3000] = {'1','0','1','0','1','0','1','0','1','0','1','1','1','1','1','1','1','1','1','1'};
+char result[BUFFER_SIZE] = {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'};
 int counter = 20;
 
-void chartobin(char c)
-{
+void chartobin(char c) {
     int i;
-    for(i = 7; i >= 0; i--){
-        result[counter] = (c & (1 << i)) ? '1' : '0';  
+    for (i = 7; i >= 0; i--) {
+        result[counter] = (c & (1 << i)) ? '1' : '0';
         counter++;
     }
 }
 
-void int2bin(unsigned integer, int n)
-{  
-    for (int i = 0; i < n; i++)   
-    {
-        result[counter] = (integer & (1 << (n - i - 1))) ? '1' : '0';
+void int2bin(unsigned integer, int n) {
+    for (int i = 0; i < n; i++) {
+        result[counter] = (integer & (int)1 << (n - i - 1)) ? '1' : '0';
         result[36] = '\0';
         counter++;
     }
@@ -32,79 +29,74 @@ void int2bin(unsigned integer, int n)
 
 int pos = 0;
 
-int main()
-{
+int main() {
     struct timeval tval_before, tval_after, tval_result;
-    struct gpiod_chip *chip;
-    struct gpiod_line *line;
-    
-    chip = gpiod_chip_open_by_name(GPIO_CHIP_NAME);
+
+    // Open GPIO chip
+    struct gpiod_chip *chip = gpiod_chip_open(GPIO_CHIP_NAME);
     if (!chip) {
         perror("Error opening GPIO chip");
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    line = gpiod_chip_get_line(chip, GPIO_PIN_NUMBER);
+    // Get GPIO line
+    struct gpiod_line *line = gpiod_chip_get_line(chip, GPIO_PIN_NUMBER);
     if (!line) {
         perror("Error getting GPIO line");
         gpiod_chip_close(chip);
-        return EXIT_FAILURE;
+        return 1;
     }
 
-    if (gpiod_line_request_output(line, "gpio-output", 0) < 0) {
-        perror("Error setting GPIO line as output");
+    // Request output direction for GPIO pin
+    if (gpiod_line_request_output(line, "output", 0) < 0) {
+        perror("Error setting GPIO pin as output");
         gpiod_line_release(line);
         gpiod_chip_close(chip);
-        return EXIT_FAILURE;
+        return 1;
     }
 
     // Read message
-    char msg[3000];
+    char msg[BUFFER_SIZE];
     int len, k, length;
 
     printf("\nEnter the Message: ");
-    scanf("%[^'\n']", msg);
+    scanf("%[^\n]", msg);
 
     len = strlen(msg);
 
     int2bin(len * 8, 16); // Multiply by 8 because one byte is 8 bits
-    printf("Frame Header (Synchro and Textlength = %s\n", result);
+    printf("Frame Header (Synchro and Textlength): %s\n", result);
 
-    for (k = 0; k < len; k++)
-    {
+    for (k = 0; k < len; k++) {
         chartobin(msg[k]);
     }
 
     length = strlen(result);
     gettimeofday(&tval_before, NULL);
-    while (pos != length)
-    {
+    while (pos != length) {
         gettimeofday(&tval_after, NULL);
         timersub(&tval_after, &tval_before, &tval_result);
         double time_elapsed = (double)tval_result.tv_sec + ((double)tval_result.tv_usec / 1000000.0f);
 
-        while (time_elapsed < 0.001)
-        {
+        while (time_elapsed < 0.001) {
             gettimeofday(&tval_after, NULL);
             timersub(&tval_after, &tval_before, &tval_result);
             time_elapsed = (double)tval_result.tv_sec + ((double)tval_result.tv_usec / 1000000.0f);
         }
         gettimeofday(&tval_before, NULL);
 
-        if (result[pos] == '1')
-        {
+        if (result[pos] == '1') {
             gpiod_line_set_value(line, 1);
             pos++;
-        }
-        else if (result[pos] == '0')
-        {
+        } else if (result[pos] == '0') {
             gpiod_line_set_value(line, 0);
             pos++;
         }
     }
 
+    // Release resources
     gpiod_line_release(line);
     gpiod_chip_close(chip);
 
-    return EXIT_SUCCESS;
+    return 0;
 }
