b800bb7905fb91ad04f5c79db76ecc916a665c51
Sreeram K R <krsreeram007@gmail.com>
2024-03-30 17:29:06 +0530

    Update msgsend.c

--- msgsend.c
+++ msgsend.c
@@ -4,25 +4,12 @@
 #include <string.h>
 #include <gpiod.h>
 
-char result[3000] = {'1','0','1','0','1','0','1','0','1','0','1','1','1','1','1','1','1','1','1','1'};
+char result[3000] = {'1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'};
 int counter = 20;
 int pos = 0;
 
-
-void int2bin(unsigned integer, int n)
-{  
-    for (int i = 0; i < n; i++)   
-    {
-        result[counter] = (integer & (int)1 << (n - i - 1)) ? '1' : '0';
-        result[36] = '\0';
-        counter++;
-    }
-}
-
-int main()
-{
+int main() {
     struct timeval tval_before, tval_after, tval_result;
-
     // GPIO Initialization
     struct gpiod_chip *chip;
     struct gpiod_line *line;
@@ -48,38 +35,32 @@ int main()
     char msg[3000];
     int len, k, length;
     gpiod_line_set_value(line, 0);
-
     printf("\n Enter the Message: ");
-    scanf("%[^'\n']", msg);
-
-    for(int i = 0 ; i<8 ; i++){
-        
+    scanf("%[^\n]", msg);
+    length = strlen(msg);
+    int copy_len = (length > 8) ? 8 : length;
+    for(int i = 0; i < copy_len; i++) {
         result[21+i] = msg[i];
-    
-    }    
-    printf ("Frame Header (Synchro and Text) = %s\n", result);
+    }
+    result[21+copy_len] = '\0'; // Add null terminator
+    printf("Frame Header (Synchro and Text) = %s\n", result);
+
     length = strlen(result);
     gettimeofday(&tval_before, NULL);
-    while(pos != length)
-    {
+    while(pos != length) {
         gettimeofday(&tval_after, NULL);
         timersub(&tval_after, &tval_before, &tval_result);
         double time_elapsed = (double)tval_result.tv_sec + ((double)tval_result.tv_usec / 1000000.0f);
-
-        while(time_elapsed < 0.001)
-        {
+        while(time_elapsed < 0.001) {
             gettimeofday(&tval_after, NULL);
             timersub(&tval_after, &tval_before, &tval_result);
             time_elapsed = (double)tval_result.tv_sec + ((double)tval_result.tv_usec / 1000000.0f);
         }
         gettimeofday(&tval_before, NULL);
-
-        if (result[pos] == '1')
-        {
+        if (result[pos] == '1') {
             gpiod_line_set_value(line, 1);
             pos++;
-        }
-        else if(result[pos] == '0') {
+        } else if(result[pos] == '0') {
             gpiod_line_set_value(line, 0);
             pos++;
         }
@@ -89,6 +70,5 @@ int main()
     gpiod_line_set_value(line, 0);
     gpiod_line_release(line);
     gpiod_chip_close(chip);
-
     return 0;
 }
